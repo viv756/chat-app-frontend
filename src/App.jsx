9c06e61a7a55a7e147d24e09498d7d6364930b03
*vivek <vrvivekofficial@gmail.com>
2024-10-09 09:55:37 +0530

    added code splitting in app

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
-import React from "react";
+import React, { lazy } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
+
+const Home = lazy(() => import("./pages/Home"));
+const Login = lazy(() => import("./pages/Login"));
+const Chat = lazy(() => import("./pages/Chat"));
+const Groups = lazy(() => import("./pages/Groups"));
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home/>} />
-        <Route path="/about" element={<h1>About</h1>} />
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/groups" element={<Groups />} />
+        <Route path="chat/:chatId" element={<Chat />} />
       </Routes>
     </BrowserRouter>
   );
