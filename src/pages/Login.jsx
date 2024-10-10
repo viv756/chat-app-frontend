980c23b9c3443821aaf5503835ea9c4e050340bc
*vivek <vrvivekofficial@gmail.com>
2024-10-10 10:42:53 +0530

    created login and register page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,146 @@
-import React from "react";
+import React, { useState } from "react";
+import {
+  Avatar,
+  Button,
+  Container,
+  IconButton,
+  Paper,
+  Stack,
+  TextField,
+  Typography,
+} from "@mui/material";
+import { CameraAlt as CameraAltIcon } from "@mui/icons-material";
+import { VisuallyHiddenInput } from "../components/styles/StyledComponent";
+import { useInputValidation } from "6pp";
 
 const Login = () => {
-  return <div>Login</div>;
+  const [isLogin, setIsLogin] = useState(true);
+
+  const toggleLogin = (prev) => setIsLogin((prev) => !prev);
+
+  const name = useInputValidation("");
+  return (
+    <Container
+      component={"main"}
+      maxWidth="xs"
+      sx={{
+        height: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}>
+      <Paper
+        elevation={3}
+        sx={{ padding: 4, display: "flex", flexDirection: "column", alignItems: "center" }}>
+        {isLogin ? (
+          <>
+            <Typography variant="h5">Login</Typography>
+            <form
+              style={{
+                width: "100%",
+                marginTop: "1rem",
+              }}>
+              <Stack position={"relative"} width={"10rem"} margin={"auto"}>
+                <Avatar
+                  sx={{
+                    width: "10rem",
+                    height: "10rem",
+                    objectFit: "contain",
+                  }}
+                />
+                <IconButton
+                  sx={{
+                    position: "absolute",
+                    bottom: "0",
+                    right: "0",
+                    color: "white",
+                    bgcolor: "rgba(0,0,0,0.5)",
+                    ":hover": {
+                      bgcolor: "rgba(0,0,0,0.7)",
+                    },
+                  }}
+                  component="label">
+                  <>
+                    <CameraAltIcon />
+                    <VisuallyHiddenInput type="file" />
+                  </>
+                </IconButton>
+              </Stack>
+
+              <TextField required fullWidth label="Username" margin="normal" variant="outlined" />
+              <TextField
+                required
+                fullWidth
+                label="Password"
+                type="password"
+                margin="normal"
+                variant="outlined"
+              />
+              <Button
+                sx={{
+                  marginTop: "1rem",
+                }}
+                variant="contained"
+                color="primary"
+                type="submit"
+                fullWidth>
+                Login
+              </Button>
+              <Typography textAlign={"center"} m={"1rem"}>
+                OR
+              </Typography>
+
+              <Button fullWidth variant="text" onClick={toggleLogin}>
+                Sign Up Instead
+              </Button>
+            </form>
+          </>
+        ) : (
+          <>
+            <Typography variant="h5">Sign Up</Typography>
+            <form
+              style={{
+                width: "100%",
+                marginTop: "1rem",
+              }}>
+              <TextField required fullWidth label="Name" margin="normal" variant="outlined" />
+
+              <TextField required fullWidth label="Bio" margin="normal" variant="outlined" />
+              <TextField required fullWidth label="Username" margin="normal" variant="outlined" />
+
+              <TextField
+                required
+                fullWidth
+                label="Password"
+                type="password"
+                margin="normal"
+                variant="outlined"
+              />
+
+              <Button
+                sx={{
+                  marginTop: "1rem",
+                }}
+                variant="contained"
+                color="primary"
+                type="submit"
+                fullWidth>
+                Sign Up
+              </Button>
+
+              <Typography textAlign={"center"} m={"1rem"}>
+                OR
+              </Typography>
+
+              <Button fullWidth variant="text" onClick={toggleLogin}>
+                Login Instead
+              </Button>
+            </form>
+          </>
+        )}
+      </Paper>
+    </Container>
+  );
 };
 
 export default Login;
